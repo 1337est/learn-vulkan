3fa25583dbf733f9993be73589725e7bc22e9c1d
1337est <59178218+1337est@users.noreply.github.com>
2024-09-03 17:07:36 -0500

    optimizing device memory through index and staging buffers

--- vkGameEngine/vge_model.hpp
+++ vkGameEngine/vge_model.hpp
@@ -30,7 +30,13 @@ public:
         getAttributeDescriptions();
     };
 
-    VgeModel(VgeDevice& device, const std::vector<Vertex>& vertices);
+    struct Builder
+    {
+        std::vector<Vertex> vertices{};
+        std::vector<uint32_t> indices{};
+    };
+
+    VgeModel(VgeDevice& device, const VgeModel::Builder& builder);
     ~VgeModel();
 
     VgeModel(const VgeModel&) = delete;
@@ -41,11 +47,18 @@ public:
 
 private:
     void createVertexBuffers(const std::vector<Vertex>& vertices);
+    void createIndexBuffers(const std::vector<uint32_t>& indices);
+
     VgeDevice& m_vgeDevice;
+
     VkBuffer m_vertexBuffer;
     VkDeviceMemory m_vertexBufferMemory;
     uint32_t m_vertexCount;
-};
 
+    bool m_hasIndexBuffer = false;
+    VkBuffer m_indexBuffer;
+    VkDeviceMemory m_indexBufferMemory;
+    uint32_t m_indexCount;
+};
 
 } // namespace vge
